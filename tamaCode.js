a67c478f8e388750e52e53cd66d4d01dd5d997e7
onefootfarina <clichecontent@gmail.com>
2022-04-15 23:41:29 -0400

    added basically the whole core game mechanics, with no good gui. this is decent so far tho i have to just touch up the random parts in it to make them more random and not as usual

--- tamaCode.js
+++ tamaCode.js
@@ -1,12 +1,32 @@
-let count = 58;
+let count = 0;
 let minCount = 0;
 let hourCount = 0;
-let tamaName = 'Tama';
+let tamaState = {
+    1: 'Egg',
+    2: 'Child_1',
+    3: 'Child_2',
+    4: 'Teen_1',
+    5: 'Teen_2',
+    6: 'Adult_1',
+    7: 'Adult_2',
+    8: 'Adult_3',
+    9: 'Adult_4',
+};
+let tamaName = 'Larry';
 let tamaAge = 0;
 let tamaHealth = 5;
-let tamaHappy = 2;
+let tamaHappy = 4;
 let tamaPoop = 0;
 let tamaSick = false;
+let timeState = {
+    gameStart: new Date(),
+    lastInteract: new Date(),
+    lastPoop: new Date(),
+    lastSick: new Date(),
+    lastHealth: new Date(),
+};
+
+console.log(tamaState[9]);
 
 const state = { lastCheckedTime: new Date() };
 state.lastCheckedTime = new Date();
@@ -18,28 +38,41 @@ body.style.backgroundColor = 'red';
 const mealButton = document.querySelector('#mealButton');
 mealButton.addEventListener('click', function () {
     feed(1);
+    timeState.lastInteract = new Date();
 });
 const snackButton = document.querySelector('#snackButton');
 snackButton.addEventListener('click', function () {
     feed(2);
+    timeState.lastInteract = new Date();
 });
 
 const healButton = document.querySelector('#healButton');
 healButton.addEventListener('click', function () {
     heal();
+    timeState.lastInteract = new Date();
 });
 const cleanButton = document.querySelector('#cleanButton');
 cleanButton.addEventListener('click', function () {
     clean();
+    timeState.lastInteract = new Date();
 });
 
 const playButton = document.querySelector('#playButton');
 playButton.addEventListener('click', function () {
     playGame();
+    timeState.lastInteract = new Date();
 });
+
+const nameButton = document.querySelector('#nameButton');
+nameButton.addEventListener('click', function () {
+    tamaName = prompt('what do u wanna name it?', 'larry');
+});
+
+const tamaText = document.querySelector('#tamaText');
 
 function start() {
     setInterval(gameLoop, 1000);
+    timeState.gameStart = new Date();
 }
 
 function gameLoop() {
@@ -51,30 +84,135 @@ function gameLoop() {
         sick: tamaSick,
         poop: tamaPoop,
     };
-
+    timeKeeping();
     getSick();
     givePoop();
     ifSick();
+    autoAge();
+    autoDegenTwo();
+    autoUnHappy();
+    // console.log(Math.floor((new Date() - timeState.gameStart) / 1000));
     // timeKeeping();
-    console.log(
+    tamaText.textContent =
         tamagotchi.name +
-            "'s: " +
-            'Happy: ' +
-            tamagotchi.happy +
-            ' Health: ' +
-            tamagotchi.health +
-            ' is sick? ' +
-            tamagotchi.sick +
-            '. ' +
-            tamagotchi.poop +
-            ' Poops'
-    );
+        ': ' +
+        'Happy: ' +
+        tamagotchi.happy +
+        ' Health: ' +
+        tamagotchi.health +
+        ' is sick? ' +
+        tamagotchi.sick +
+        '. ' +
+        tamagotchi.poop +
+        ' Poops';
 }
 start();
 
 function randomNumGen(percent) {
     let randomNum = Math.floor(Math.random() * percent);
     return randomNum;
+}
+
+function autoDegenTwo() {
+    if (Math.floor((new Date() - timeState.gameStart) / 1000) % 2 == 0) {
+        let randomNum = randomNumGen(2000);
+        if (tamaHappy <= 2) {
+            //not happy more health loss
+            if (
+                (randomNum > 10 && randomNum < 100) ||
+                (randomNum > 800 && randomNum < 900)
+            ) {
+                tamaHealth--;
+                timeState.lastHealth = new Date();
+            }
+        } else if (
+            Math.floor((new Date() - timeState.lastHealth) / 1000) > 35
+        ) {
+            if (randomNum > 10 && randomNum < 100) {
+                tamaHealth--;
+                timeState.lastHealth = new Date();
+            }
+        }
+    }
+}
+
+function autoDegen() {
+    let randomNum = randomNumGen(200);
+    if (tamaHappy <= 2) {
+        //IF UNHAPPY WILL LOOSE HEALTH FASTER
+        if (randomNum >= 50 && randomNum <= 105) {
+            tamaHealth--;
+        }
+    }
+    //if 5 min from interacting will have less chance to loose health
+    if (Math.floor((new Date() - timeState.lastInteract) / 1000) < 300) {
+        if (randomNum >= 10 && randomNum <= 30) {
+            tamaHealth--;
+        }
+    } else {
+        //if u dont interact for 5 min
+        if (randomNum == 69) {
+            tamaHealth--;
+        }
+        //30 sex
+        if (Math.floor((new Date() - timeState.gameStart) / 1000) % 30 == 0) {
+            //10% chance to decrease
+            if (randomNum >= 40 && randomNum <= 70) {
+                tamaHealth--;
+            }
+        } //5 min
+        else if (
+            Math.floor((new Date() - timeState.gameStart) / 1000) % 300 ==
+            0
+        ) {
+            //40% chance to decrease
+            if (randomNum >= 30 && randomNum <= 80) {
+                tamaHealth--;
+            }
+        } //10 min
+        else if (
+            Math.floor((new Date() - timeState.gameStart) / 1000) % 600 ==
+            0
+        ) {
+            //60% chance to decrease
+            if (randomNum >= 20 && randomNum <= 120) {
+                tamaHealth--;
+            }
+        }
+    }
+}
+
+function autoUnHappy() {
+    let randomNum = randomNumGen(200);
+    if (randomNum == 69) {
+        tamaHealth--;
+    }
+    //30 sex
+    if (Math.floor((new Date() - timeState.gameStart) / 1000) % 30 == 0) {
+        //5% chance to decrease
+        if (randomNum >= 38 && randomNum <= 47) {
+            tamaHealth--;
+        }
+    } //5 min
+    else if (Math.floor((new Date() - timeState.gameStart) / 1000) % 300 == 0) {
+        //40% chance to decrease
+        if (randomNum >= 10 && randomNum <= 40) {
+            tamaHealth--;
+        }
+    } //10 min
+    else if (Math.floor((new Date() - timeState.gameStart) / 1000) % 600 == 0) {
+        //60% chance to decrease
+        if (randomNum >= 10 && randomNum <= 90) {
+            tamaHealth--;
+        }
+    }
+}
+
+function autoAge() {
+    //1 day
+    if (Math.floor((new Date() - timeState.gameStart) / 1000) % 86400 == 0) {
+        tamaAge++;
+    }
 }
 
 //1 == meal && 2 == snack
@@ -98,8 +236,26 @@ function feed(type) {
 }
 
 function playGame() {
-    tamaHappy = tamaHappy + 1;
-
+    let score = 0;
+    for (let i = 0; i < 5; i++) {
+        let compChoice = randomNumGen(2);
+        let playerChoice = prompt('Best out of 3: 0 or 1');
+        if (playerChoice == compChoice) {
+            alert('nice you got it!');
+            score++;
+        } else {
+            alert('woops they chose' + compChoice);
+        }
+        if (score > 2) {
+            break;
+        }
+    }
+    if (score > 2) {
+        alert('you won!!! +1 happy');
+        tamaHappy = tamaHappy + 1;
+    } else {
+        alert('you lost! try again no happy gained');
+    }
     if (tamaHappy == 5 || tamaHappy > 5) {
         tamaHappy = 5;
     }
@@ -118,11 +274,13 @@ function givePoop() {
                 randomNum == 120
             ) {
                 tamaPoop += 1;
+                tamaHealth -= 1;
             }
         } else {
             let randomNum2 = randomNumGen(3000);
             if (randomNum2 == 1212 || randomNum2 == 74 || randomNum2 == 2121) {
                 tamaPoop += 1;
+                tamaHealth -= 0.5;
             }
         }
     }
@@ -130,16 +288,21 @@ function givePoop() {
 
 function getSick() {
     let randomNumber = randomNumGen(5000);
+    //add a if statement that makes it so it cant get sick after just getting sick
+
     // console.log(randomNumber);
     if (randomNumber == 8 || randomNumber == 573 || randomNumber == 1362) {
         tamaSick = true;
+        timeState.lastSick = new Date();
     }
     return randomNumber;
 }
 
 function ifSick() {
     if (tamaSick == true) {
-        tamaHealth = tamaHealth - 0.1;
+        if (Math.floor((new Date() - timeState.lastSick) / 1000) % 2 == 0) {
+            tamaHealth -= 0.5;
+        }
     }
 }
 
@@ -151,12 +314,13 @@ function clean() {
     tamaPoop = 0;
 }
 
+const timeText = document.querySelector('#timeText');
 function timeKeeping() {
     count++;
     if (count > 1) {
-        console.log(`${hourCount} Hour ${minCount} Min ${count} seconds`);
+        timeText.textContent = `${hourCount} Hour ${minCount} Min ${count} seconds`;
     } else {
-        console.log(`${hourCount} Hour ${minCount} Min ${count} seconds`);
+        timeText.textContent = `${hourCount} Hour ${minCount} Min ${count} seconds`;
     }
     if (count % 60 == 0) {
         minCount++;
@@ -168,7 +332,7 @@ function timeKeeping() {
             hourCount++;
             minCount = 0;
             count = 0;
-            console.log(minCount);
+            timeText.textContent = minCount;
         }
     }
 }
